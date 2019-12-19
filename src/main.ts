6daca993f4164c62f110c5d664891f7425603b6b
Oliver <rift95@live.se>
2019-12-19 18:05:37 +0100

    ran vue add pwa

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'normalize.css';
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
   outputDir: 'dist',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/get-shit-done/'
-    : '/'
+    : '/',
+  pwa: {}
 };
